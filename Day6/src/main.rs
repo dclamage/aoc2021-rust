89e92cd3e2700f1d8b84b085fdd978e2c27d3f29
David Clamage <dclamage@users.noreply.github.com>
2021-12-04 22:50:56 -0800

    Day6 template

---
+++ Day6/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::time;
+
+fn part1(_lines: &[&str]) -> u32 {
+    0
+}
+
+fn part2(_lines: &[&str]) -> u32 {
+    0
+}
+
+fn main() {
+    // Read all lines of input.txt
+    let read_time_start = time::Instant::now();
+    //let input = fs::read_to_string("example.txt").expect("Unable to read file");
+    let input = fs::read_to_string("input.txt").expect("Unable to read file");
+    let lines: Vec<&str> = input.lines().collect();
+    let read_time = read_time_start.elapsed();
+
+    println!("Part 1:");
+    let part1_time_start = time::Instant::now();
+    let answer = part1(&lines);
+    let part1_time = part1_time_start.elapsed();
+    println!("Part 1: Answer: {}", answer);
+
+    println!("");
+    println!("----------------------------------");
+    println!("");
+
+    println!("Part 2:");
+    let part2_time = time::Instant::now();
+    let answer = part2(&lines);
+    let part2_time = part2_time.elapsed();
+    println!("Part 2: Answer: {}", answer);
+
+    println!("");
+    println!("----------------------------------");
+    println!("");
+
+    println!("Read time: {:?}", read_time);
+    println!("Part 1 time: {:?}", part1_time);
+    println!("Part 2 time: {:?}", part2_time);
+}
