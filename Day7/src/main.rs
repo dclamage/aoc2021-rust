7e80ee44809379932d5a14a02703f4be3aee8ce5
David Clamage <dclamage@users.noreply.github.com>
2021-12-05 23:52:58 -0800

    Day 7 Template

---
+++ Day7/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::time;
+
+fn part1(_input: &[i32]) -> u64 {
+    0
+}
+
+fn part2(_input: &[i32]) -> u64 {
+    0
+}
+
+fn main() {
+    // Read all lines of input.txt
+    let read_time_start = time::Instant::now();
+    //let input = fs::read_to_string("example.txt").expect("Unable to read file");
+    let input = fs::read_to_string("input.txt").expect("Unable to read file");
+    let input: Vec<i32> = input.split(',').map(|s| s.trim().parse().unwrap()).collect();
+    let read_time = read_time_start.elapsed();
+
+    println!("Part 1:");
+    let part1_time_start = time::Instant::now();
+    let answer = part1(&input);
+    let part1_time = part1_time_start.elapsed();
+    println!("Part 1: Answer: {}", answer);
+
+    println!("");
+    println!("----------------------------------");
+    println!("");
+
+    println!("Part 2:");
+    let part2_time = time::Instant::now();
+    let answer = part2(&input);
+    let part2_time = part2_time.elapsed();
+    println!("Part 2: Answer: {}", answer);
+
+    println!("");
+    println!("----------------------------------");
+    println!("");
+
+    println!("Read time: {:?}", read_time);
+    println!("Part 1 time: {:?}", part1_time);
+    println!("Part 2 time: {:?}", part2_time);
+}
