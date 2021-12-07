ecf9730fd74bc3f7f651d896ba290187759e7a34
David Clamage <dclamage@users.noreply.github.com>
2021-12-06 21:08:58 -0800

    Day 7 Initial solution parts 1 & 2

--- Day7/src/main.rs
+++ Day7/src/main.rs
@@ -1,12 +1,63 @@
 use std::fs;
 use std::time;
 
-fn part1(_input: &[i32]) -> u64 {
-    0
+fn part1(crab_loc: &[i32]) -> u64 {
+    let (min, max) = min_max_loc(&crab_loc);
+    let mut center_loc = min;
+    let mut best_cost = i32::max_value();
+    while center_loc != max {
+        let cost = eval_cost(crab_loc, center_loc);
+        if cost < best_cost {
+            best_cost = cost;
+        }
+        center_loc += 1;
+    }
+    best_cost as u64
 }
 
-fn part2(_input: &[i32]) -> u64 {
-    0
+fn part2(crab_loc: &[i32]) -> u64 {
+    let (min, max) = min_max_loc(&crab_loc);
+    let mut center_loc = min;
+    let mut best_cost = i32::max_value();
+    while center_loc != max {
+        let cost = eval_cost2(crab_loc, center_loc);
+        if cost < best_cost {
+            best_cost = cost;
+        }
+        center_loc += 1;
+    }
+    best_cost as u64
+}
+
+fn min_max_loc(crab_loc: &[i32]) -> (i32, i32) {
+    let mut min = i32::max_value();
+    let mut max = i32::min_value();
+    for loc in crab_loc {
+        if *loc < min {
+            min = *loc;
+        }
+        if *loc > max {
+            max = *loc;
+        }
+    }
+    (min, max)
+}
+
+fn eval_cost(crab_loc: &[i32], center_loc: i32) -> i32 {
+    let mut cost = 0;
+    for loc in crab_loc {
+        cost += (*loc - center_loc).abs();
+    }
+    cost
+}
+
+fn eval_cost2(crab_loc: &[i32], center_loc: i32) -> i32 {
+    let mut cost = 0;
+    for loc in crab_loc {
+        let dist = (*loc - center_loc).abs();
+        cost += (dist * (dist + 1)) / 2;
+    }
+    cost
 }
 
 fn main() {
@@ -14,7 +65,10 @@ fn main() {
     let read_time_start = time::Instant::now();
     //let input = fs::read_to_string("example.txt").expect("Unable to read file");
     let input = fs::read_to_string("input.txt").expect("Unable to read file");
-    let input: Vec<i32> = input.split(',').map(|s| s.trim().parse().unwrap()).collect();
+    let input: Vec<i32> = input
+        .split(',')
+        .map(|s| s.trim().parse().unwrap())
+        .collect();
     let read_time = read_time_start.elapsed();
 
     println!("Part 1:");
