124016d70f69ea8336701c885aeb2dff9288cd4d
David Clamage <dclamage@users.noreply.github.com>
2021-12-14 23:29:24 -0800

    Part 1 working

--- Day15/src/main.rs
+++ Day15/src/main.rs
@@ -1,28 +1,96 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs;
 use std::time;
 
 type CostMap = HashMap<(usize, usize), usize>;
-type VisitedMap = HashSet<(usize, usize)>;
 
-fn get_cost(costs: &CostMap, x: usize, y: usize) -> usize {
-    *costs.get(&(x, y)).unwrap_or(&0)
+struct AdjacentIterator {
+    base_i: usize,
+    base_j: usize,
+    width: usize,
+    height: usize,
+    adjacent_index: usize,
 }
 
-struct StackEntry {
+impl AdjacentIterator {
+    fn new(base_i: usize, base_j: usize, width: usize, height: usize) -> AdjacentIterator {
+        AdjacentIterator {
+            base_i,
+            base_j,
+            width,
+            height,
+            adjacent_index: 0,
+        }
+    }
+}
+
+impl Iterator for AdjacentIterator {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.adjacent_index {
+            0 => {
+                self.adjacent_index += 1;
+                if self.base_i > 0 {
+                    Some((self.base_i - 1, self.base_j))
+                } else {
+                    self.next()
+                }
+            }
+            1 => {
+                self.adjacent_index += 1;
+                if self.base_j > 0 {
+                    Some((self.base_i,self.base_j - 1))
+                } else {
+                    self.next()
+                }
+            }
+            2 => {
+                self.adjacent_index += 1;
+                if self.base_i < self.height - 1 {
+                    Some((self.base_i + 1, self.base_j))
+                } else {
+                    self.next()
+                }
+            }
+            3 => {
+                self.adjacent_index += 1;
+                if self.base_j < self.width - 1 {
+                    Some((self.base_i, self.base_j + 1))
+                } else {
+                    self.next()
+                }
+            }
+            _ => None,
+        }
+    }
+}
+
+fn adj(i: usize, j: usize, width: usize, height: usize) -> AdjacentIterator {
+    AdjacentIterator::new(i, j, width, height)
+}
+
+fn get_cost(costs: &CostMap, p: &(usize, usize)) -> usize {
+    *costs.get(&p).unwrap_or(&0)
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Node {
     x: usize,
     y: usize,
+    prev_x: usize,
+    prev_y: usize,
     cost: usize,
-    visited: HashSet<(usize, usize)>,
 }
 
-impl StackEntry {
-    fn new(x: usize, y: usize, cost: usize, visited: VisitedMap) -> StackEntry {
-        StackEntry {
+impl Node {
+    fn new(x: usize, y: usize) -> Node {
+        Node {
             x,
             y,
-            cost,
-            visited,
+            prev_x: usize::max_value(),
+            prev_y: usize::max_value(),
+            cost: usize::max_value(),
         }
     }
 }
@@ -30,66 +98,36 @@ impl StackEntry {
 fn part1(costs: &CostMap) -> usize {
     let max_x = costs.iter().map(|(k, _)| k.0).max().unwrap();
     let max_y = costs.iter().map(|(k, _)| k.1).max().unwrap();
-    let mut stack: Vec<StackEntry> = Vec::new();
-    {
-        let mut visited = HashSet::new();
-        visited.insert((0, 0));
-        stack.push(StackEntry {
-            x: 0,
-            y: 0,
-            cost: 0,
-            visited,
-        });
+    let mut nodes: HashMap<(usize, usize), Node> = HashMap::new();
+    for p in costs.keys() {
+        nodes.insert(*p, Node::new(p.0, p.1));
     }
-    let mut best_cost: usize = usize::max_value();
-    while !stack.is_empty() {
-        let entry = stack.pop().unwrap();
-        if entry.x == max_x && entry.y == max_y {
-            best_cost = entry.cost.min(best_cost);
-            continue;
-        }
-        if entry.cost >= best_cost {
-            continue;
-        }
+    nodes.get_mut(&(0, 0)).unwrap().cost = 0;
+    
+    let mut unvisited_nodes: Vec<(usize, usize)> = nodes.keys().map(|k| *k).collect();
+    while !unvisited_nodes.is_empty() {
+        let (node_index, node_pos) = unvisited_nodes.iter().enumerate().min_by_key(|(_, p)| {
+            nodes.get(p).unwrap().cost
+        }).unwrap();
+        let node_pos = node_pos.clone();
+        unvisited_nodes.remove(node_index);
 
-        let mut new_visited = entry.visited.clone();
-        new_visited.insert((entry.x, entry.y));
+        let node = nodes.get(&node_pos).unwrap().clone();
 
-        if entry.x < max_x && !entry.visited.contains(&(entry.x + 1, entry.y)) {
-            stack.push(StackEntry::new(
-                entry.x + 1,
-                entry.y,
-                entry.cost + get_cost(costs, entry.x + 1, entry.y),
-                new_visited.clone(),
-            ));
-        }
-        if entry.y < max_y && !entry.visited.contains(&(entry.x, entry.y + 1)) {
-            stack.push(StackEntry::new(
-                entry.x,
-                entry.y + 1,
-                entry.cost + get_cost(costs, entry.x, entry.y + 1),
-                new_visited.clone(),
-            ));
-        }
-        if entry.x > 0 && !entry.visited.contains(&(entry.x - 1, entry.y)) {
-            stack.push(StackEntry::new(
-                entry.x - 1,
-                entry.y,
-                entry.cost + get_cost(costs, entry.x - 1, entry.y),
-                new_visited.clone(),
-            ));
-        }
-        if entry.y > 0 && !entry.visited.contains(&(entry.x, entry.y - 1)) {
-            stack.push(StackEntry::new(
-                entry.x,
-                entry.y - 1,
-                entry.cost + get_cost(costs, entry.x, entry.y - 1),
-                new_visited.clone(),
-            ));
+        for adj_pos in adj(node.x, node.y, max_x + 1, max_y + 1) {
+            if unvisited_nodes.contains(&adj_pos) {
+                let new_cost = node.cost + get_cost(costs, &adj_pos);
+                let next_node = nodes.get_mut(&adj_pos).unwrap();
+                if new_cost < next_node.cost {
+                    next_node.cost = new_cost;
+                    next_node.prev_x = node.x;
+                    next_node.prev_y = node.y;
+                }
+            }
         }
     }
 
-    best_cost
+    nodes.get(&(max_x, max_y)).unwrap().cost
 }
 
 fn part2(_input: &CostMap) -> usize {
@@ -113,8 +151,8 @@ fn parse_costs(input: &str) -> CostMap {
 fn main() {
     // Read all lines of input.txt
     let read_time_start = time::Instant::now();
-    let input = fs::read_to_string("example.txt").expect("Unable to read file");
-    //let input = fs::read_to_string("input.txt").expect("Unable to read file");
+    //let input = fs::read_to_string("example.txt").expect("Unable to read file");
+    let input = fs::read_to_string("input.txt").expect("Unable to read file");
     let input = parse_costs(&input);
 
     let read_time = read_time_start.elapsed();
