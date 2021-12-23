b25d6a0b205ada22dfba25762b27d72b62a43e7f
David Clamage <dclamage@users.noreply.github.com>
2021-12-23 00:01:50 -0800

    Day 24 Template

---
+++ Day24/src/main.rs
@@ -0,0 +1,47 @@
+#[allow(unused)]
+use itertools::Itertools;
+#[allow(unused)]
+use regex::Regex;
+use std::fs;
+use std::time;
+
+fn part1(_input: &str) -> i64 {
+    0
+}
+
+fn part2(_input: &str) -> i64 {
+    0
+}
+
+fn main() {
+    // Read all lines of input.txt
+    let read_time_start = time::Instant::now();
+    //let input = fs::read_to_string("example.txt").expect("Unable to read file");
+    let input = fs::read_to_string("input.txt").expect("Unable to read file");
+
+    let read_time = read_time_start.elapsed();
+
+    println!("Part 1:");
+    let part1_time_start = time::Instant::now();
+    let answer = part1(&input);
+    let part1_time = part1_time_start.elapsed();
+    println!("Part 1: Answer: {}", answer);
+
+    println!("");
+    println!("----------------------------------");
+    println!("");
+
+    println!("Part 2:");
+    let part2_time = time::Instant::now();
+    let answer = part2(&input);
+    let part2_time = part2_time.elapsed();
+    println!("Part 2: Answer: {}", answer);
+
+    println!("");
+    println!("----------------------------------");
+    println!("");
+
+    println!("Read time: {:?}", read_time);
+    println!("Part 1 time: {:?}", part1_time);
+    println!("Part 2 time: {:?}", part2_time);
+}
